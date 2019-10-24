b8ea164155cea5a5cff53a8c5ed5b061426f38aa
Serenity <limingwei12@foxmail.com>
2019-10-24 09:17:51 +0800

    1.细化存储路径在Data目录下创建当天日期的目录,来存放当天下载的文件,方便查找. 2.添加打开下载目录的功能,打开当天的下载存放目录.

--- Pages/ShellView.xaml.cs
+++ Pages/ShellView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,7 +36,7 @@ namespace WebResourceHookWpf.Pages
         /// </summary>
         public const string CommonUrlPropertyName = "CommonUrl";
 
-        private string _commonUrl;
+        private string _commonUrl = "https://developer.api.autodesk.com/modelderivative/v2/viewers/7.*/";
 
         /// <summary>
         /// Sets 和 gets CommonUrl 的属性。
@@ -90,13 +91,48 @@ namespace WebResourceHookWpf.Pages
             }
         }
 
+        /// <summary>
+        /// <see cref="DownloadPath" /> 这是属性的名称.
+        /// </summary>
+        //        public const string DownloadPathPropertyName = "DownloadPath";
+
+        //private string _downloadPath = Environment.CurrentDirectory + "/Data/" + DateTime.Now.Date.ToString("yyyyMMMMdd");//MMMM代表月份显示中文
+        private string _downloadPath = Environment.CurrentDirectory + @"\Data\" + DateTime.Now.Date.ToString("yyyyMMdd");
+
+        /// <summary>
+        /// Sets 和 gets DownloadPath 的属性。
+        /// 对该属性值的更改将引发PropertyChanged事件。
+        /// </summary>
+        public string DownloadPath
+        {
+            get
+            {
+                if (!Directory.Exists(_downloadPath))
+                {
+                    Directory.CreateDirectory(_downloadPath);
+                }
+                return _downloadPath;
+            }
+
+            /*set
+            {
+                if (_downloadPath= == value)
+                {
+                    return;
+                }
+
+                _downloadPath= = value;
+                //                RaisePropertyChanged(DownloadPathPropertyName);
+            }*/
+        }
+
         #endregion 资源绑定
 
         private void DownloadResource(object sender, RoutedEventArgs e)
         {
             string url = CommonUrl + ResourceName;
             MessageBox.Show("完整地址：" + url);
-            bool downloadFileByAria2Async = Core.DownloadFileByAria2Async(url, Environment.CurrentDirectory + "/Data");
+            bool downloadFileByAria2Async = Core.DownloadFileByAria2Async(url, DownloadPath);
             MessageBox.Show("下载是否成功：" + downloadFileByAria2Async);
         }
 
@@ -132,5 +168,11 @@ namespace WebResourceHookWpf.Pages
         }
 
         #endregion 窗口相关
+
+        private void OpenDownPath(object sender, RoutedEventArgs e)
+        {
+            //System.Diagnostics.Process.Start(v_OpenFilePath);//打开文件
+            System.Diagnostics.Process.Start("explorer.exe", DownloadPath);//打开目录
+        }
     }
 }
