286c45a960105c386818e2eb71b8ddd4d325d139
Serenity <limingwei12@foxmail.com>
2019-10-24 09:58:02 +0800

    1.更新界面主题颜色. 2.添加双击关闭程序功能(使用Stylet的事件触发)

--- Pages/ShellViewModel.cs
+++ Pages/ShellViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Windows;
 using Stylet;
 
 namespace WebResourceHookWpf.Pages
 {
     public class ShellViewModel : Screen
     {
+        public void WinClose() => Application.Current.Shutdown();    // C#6的语法, 表达式方法
     }
 }
